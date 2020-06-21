8fe9d4631f616f1ed35e79a5d39a4a3d60aeac9d
KN Mind Set <its@metrosystems.co.th>
2020-06-22 05:06:25 +0700

    Update SpeedWatcher.sh

--- SpeedWatcher.sh
+++ SpeedWatcher.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
-msg="message=$1::$2"
-curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -H "Authorization: Bearer ==========ใส่ Token Line ของตัวเองนะ=========" \
-    --data "$msg" \
-    https://notify-api.line.me/api/notify
+
+# Configuration
+SpeedBaseline=60  # Mbps
+LinkName="FTTH TRUE1"
+MSGNormal="Normal"
+MSGProblem="Problem"
+LINEAccessToken=xxxx
+
+# Test
+Date=$(date)
+SpeedDownloadResult=$(speedtest-cli | grep ^Download)
+SpeedDownload=$(echo $SpeedDownloadResult | grep -Eo '[0-9]+\.[0-9]+')
+
+
+# Print result
+if (( $(bc <<<"$SpeedDownload > $SpeedBaseline") )); then
+    MSGLINE="Result Test $LinkName | $MSGNormal | $SpeedDownloadResult"
+    echo -e "${Date} | ${MSGLINE}"
+    curl -X POST -H "Authorization: Bearer ${LINEAccessToken}" -d "message=${MSGLINE}" https://notify-api.line.me/api/notify
+else
+    MSGLINE="Result Test $LinkName | $MSGProblem | $SpeedDownloadResult"
+    echo -e "${Date} | ${MSGLINE}"
+    curl -X POST -H "Authorization: Bearer ${LINEAccessToken}" -d "message=${MSGLINE}" https://notify-api.line.me/api/notify
+fi
